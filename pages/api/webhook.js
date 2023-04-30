9b994dd1d0405faa5432a0226727a5ac449478a0
Lienkulet <clocicovalexandru@gmail.com>
2023-04-30 13:33:43 +0300

    webhook modified

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -10,44 +10,57 @@ export default async function handler(req, res) {
   await mongooseConnect();
   const sig = req.headers['stripe-signature'];
 
-  let event;
-
   try {
-    event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+    const event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret);
+
+    // Handle the event
+    switch (event.type) {
+      case 'checkout.session.completed':
+        const checkoutSessionCompleted  = event.data.object;
+
+        const total = checkoutSessionCompleted.amount_total;
+        const name = checkoutSessionCompleted.metadata.name;
+        const email = checkoutSessionCompleted.metadata.email;
+        const city = checkoutSessionCompleted.metadata.city;
+        const postalCode = checkoutSessionCompleted.metadata.postalCode;
+        const streetAddress = checkoutSessionCompleted.metadata.streetAddress;
+        const country = checkoutSessionCompleted.metadata.country;
+        const line_items = JSON.parse(checkoutSessionCompleted.metadata.items);
+        const paid = checkoutSessionCompleted.payment_status === 'paid';
+
+        // Create order
+        const order = {
+          line_items,
+          name,
+          email,
+          city,
+          postalCode,
+          streetAddress,
+          country,
+          paid,
+          total,
+        };
+
+        await Order.create(order);
+
+        const message = 'Order created successfully.';
+        console.log(message);
+
+        // Send response to client
+        res.status(200).json({ message });
+
+        break;
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
   } catch (err) {
+    console.log(`Webhook Error: ${err.message}`);
     res.status(400).send(`Webhook Error: ${err.message}`);
-    return;
   }
-
-  // Handle the event
-  switch (event.type) {
-    case 'checkout.session.completed':
-      const checkoutSessionCompleted  = event.data.object;
-
-      const total = checkoutSessionCompleted.amount_total;
-      const name = checkoutSessionCompleted.metadata.name;
-      const email = checkoutSessionCompleted.metadata.email;
-      const city = checkoutSessionCompleted.metadata.city;
-      const postalCode = checkoutSessionCompleted.metadata.postalCode;
-      const streetAddress = checkoutSessionCompleted.metadata.streetAddress;
-      const country = checkoutSessionCompleted.metadata.country;
-      const line_items = JSON.parse(checkoutSessionCompleted.metadata.items);
-      const paid = checkoutSessionCompleted.payment_status === 'paid';
-     
-      await Order.create({
-        line_items, name, email, city, postalCode, streetAddress, country, paid, total
-      })      
-
-      break;
-
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // res.status(200).send('ok');
-  response.send(200);
 }
 
-// export const config = {
-//   api: { bodyParser: false, }
-// };
+export const config = {
+  api: { bodyParser: false, }
+};
